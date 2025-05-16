aeb15527617305c66fcd7baa1fdf8efce9f6ade1
AreenHalabe <93481480+AreenHalabe@users.noreply.github.com>
2025-05-17 00:55:56 +0300

    add lexv2

--- thir-proj/src/App.js
+++ thir-proj/src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import ChatBot from './components/ChatBot';
+
+// function App() {
+//   return (
+//     <Authenticator>
+//       {({ signOut, user }) => (
+//         <main style={{ padding: '1rem' }}>
+//           <h2>Welcome, {user.username}!</h2>
+//           <ChatBot />
+//           <button onClick={signOut} style={{ marginTop: '1rem' }}>
+//             Sign Out
+//           </button>
+//         </main>
+//       )}
+//     </Authenticator>
+//   );
+// }
+
+// export default App;
+
+import LexV2Chat from './components/LexV2Chat';
 
 function App() {
   return (
@@ -9,7 +28,7 @@ function App() {
       {({ signOut, user }) => (
         <main style={{ padding: '1rem' }}>
           <h2>Welcome, {user.username}!</h2>
-          <ChatBot />
+          <LexV2Chat />
           <button onClick={signOut} style={{ marginTop: '1rem' }}>
             Sign Out
           </button>
